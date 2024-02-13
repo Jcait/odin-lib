c88ca084ab8bd7b3df3f5bf561d9133468a8a1b0
Jo <134626490+Jcait@users.noreply.github.com>
2024-02-13 16:45:43 +0000

    add new card without dupes

--- index.js
+++ index.js
@@ -10,18 +10,26 @@ myLibrary.forEach ((book) => {
 })
 
 const update = () => myLibrary.forEach((book, i) => {
-    console.log(i)
-    const newDiv = document.createElement("div")
-    const cardTitle = document.createElement("h1")
-    const author = document.createElement("h2")
-    cardTitle.innerText = book.name
-    author.innerText = book.author
-    newDiv.className = "card"
-    book.index = myLibrary.length -1
-    bookList.append(newDiv)
-    newDiv.append(cardTitle)
-    newDiv.append(author)
-    libCount++
+    // checks if book is already inserted
+    if(i == book.index) {
+        return
+    } else {
+        const newDiv = document.createElement("div")
+        const cardTitle = document.createElement("h1")
+        const author = document.createElement("h2")
+        cardTitle.innerText = book.name
+        author.innerText = book.author
+        newDiv.className = "card"
+        book.index = i
+        bookList.append(newDiv)
+        newDiv.append(cardTitle)
+        newDiv.append(author)
+        console.log(`book index: ${book.index}`)
+        libCount++
+    }
+    console.log(`index: ${i}`)
+    console.log(`libCount: ${libCount}`)
+
 })
 
 function Book(name, author, pages, read ){
