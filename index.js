3103a790687dc510957cb5bf84b6145dcacc70c3
Jo <134626490+Jcait@users.noreply.github.com>
2024-02-13 16:20:27 +0000

    testing enumeration

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-
 const newBook = document.querySelector(".new-book")
 const bookList = document.querySelector(".book-list")
+let libCount = 0
+
+let myLibrary = [{name: "test", author: "test", pages: "test", read: "test"}, 
+{name: "test", author: "test", pages: "test", read: "test"}]
+
+myLibrary.forEach ((book) => {
+    console.log(book.name)
+})
+
+const update = () => myLibrary.forEach((book, i) => {
+    console.log(i)
+    const newDiv = document.createElement("div")
+    const cardTitle = document.createElement("h1")
+    const author = document.createElement("h2")
+    cardTitle.innerText = book.name
+    author.innerText = book.author
+    newDiv.className = "card"
+    book.index = myLibrary.length -1
+    bookList.append(newDiv)
+    newDiv.append(cardTitle)
+    newDiv.append(author)
+    libCount++
+})
+
+function Book(name, author, pages, read ){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+}
 
 newBook.addEventListener("click", () => {
-    const newDiv = document.createElement("div")
-   bookList.append(newDiv)
-   newDiv.innerText = "Hello"
-   newDiv.className = "card"
+    const book = new Book("one", "two", "three", "four")
+    myLibrary.push(book)
+    update()
 })
