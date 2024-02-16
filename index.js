4d9407c1cbfb36cd573698794117145a012358c7
Jo <134626490+Jcait@users.noreply.github.com>
2024-02-16 15:17:52 +0000

    fixed extra card generate

--- index.js
+++ index.js
@@ -3,40 +3,43 @@ const bookList = document.querySelector(".book-list")
 const bookForm = document.querySelector(".add-book")
 const btnSubmit = document.querySelector(".form-submit")
 const delBtn = document.querySelector(".delete-button")
-let libCount = 0
 
-let myLibrary = [{name: "test", author: "test", pages: "test", read: "true"}, 
-{name: "test", author: "test", pages: "test", read: "false"}]
 
-myLibrary.forEach ((book) => {
-    console.log(book.name)
-})
+let myLibrary = [ ]
 
-const update = () => myLibrary.forEach((book, i) => {
+const update = function() {
+    myLibrary.forEach((book, i) => {
+    book.index = i
+    console.log(book)
+    console.log(i)
     // checks if book is already inserted
-    if(i == book.index) {
-        return
-    } else {
-        const newDiv = document.createElement("div")
+        const newDiv =  document.createElement("div")
+        newDiv.className = "card"
+
         const cardTitle = document.createElement("h2")
-        const author = document.createElement("h3")
-        const read = document.createElement("p")
-        const pages = document.createElement("p")
-        const delBtn = document.createElement("img")
-        delBtn.src = "trash-outline.svg"
         cardTitle.innerText = `Titie: ${book.name}`
+        
+        const author = document.createElement("h3")
         author.innerText = `Author: ${book.author}`
+
+        const read = document.createElement("p")
+        
+        const pages = document.createElement("p")
         if(book.read == "true") {
             read.innerText = "Read"
         } else {
             read.innerText = "Not-Read"
         }
         pages.innerText = book.pages
-        newDiv.className = "card"
        pages.className = "pages"
+
+        const delBtn = document.createElement("img")
+        read.className = "is-read"
+
+        delBtn.src = "trash-outline.svg"
+
+
        delBtn.className = "delete-button"
-       read.className = "is-read"
-        book.index = i
         bookList.append(newDiv)
         newDiv.append(cardTitle, author, read, pages,delBtn)
         delBtn.addEventListener("click", function() {
@@ -45,22 +48,16 @@ const update = () => myLibrary.forEach((book, i) => {
             myLibrary.splice(book.index, 1)
 
         })
-        // newDiv.append(author)
-        // newDiv.append(read)
-        // newDiv.append(pages)
-        console.log(`book index: ${book.index}`)
-        libCount++
-    }
-    console.log(`index: ${i}`)
-    console.log(`libCount: ${libCount}`)
 
 })
+}
 
-function Book(name, author, pages, read ){
+function Book(name, author, pages, read){
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.read = read
+    this.read = read;
+    
 }
 
 newBook.addEventListener("click", () => {
@@ -74,16 +71,23 @@ newBook.addEventListener("click", () => {
 
 
 const addToLib = function(event){
-    // no database so no default submit
     event.preventDefault()
-    const name = document.querySelector(".name").value
-    const author = document.querySelector(".author").value
-    const pages = document.querySelector(".pages").value
-    const selected = document.querySelector('input:checked').value;
-    const addB = new Book(name, author, pages, selected)
-    myLibrary.push(addB)
-    console.log(name)
-    update()
+    for(let i = 0; i < myLibrary.length; i++) {
+        if(myLibrary[i] == true && myLibrary.length != 0) {
+        } else {
+            const name = document.querySelector(".name").value
+            const author = document.querySelector(".author").value
+            const pages = document.querySelector(".pages").value
+            const selected = document.querySelector('input:checked').value;
+            const index = myLibrary.length
+            const addB = new Book(name, author, pages, selected)
+            myLibrary.push(addB)
+            console.log(name)
+            update()
+        }
+    }
+    // no database so no default submit
+
 }
 
 btnSubmit.addEventListener("click", addToLib, false)
